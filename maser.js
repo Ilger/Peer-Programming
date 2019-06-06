a0827201543b0a67021ba44d20e21125a8d11d05
ilger <ilgerdepraetere@gmail.com>
2019-06-06 22:50:20 +0200

    rename id's in all files, make more use of var's, change reslut div text output

--- maser.js
+++ maser.js
@@ -3,7 +3,8 @@
 
 let calcButton = document.getElementById('calcButton');
 let buttonStyle = document.querySelector('.btn-primary')
-let formInput = document.getElementById(formCalculate);
+let formOutput = document.getElementById('formCalculate');
+let resultInput = document.getElementById('resultInputField');
 
 
 let buttonEvent = function (executeFn) {
@@ -26,21 +27,25 @@ buttonEvent(clicky);
 let stylesToggelBtn = function () {
   buttonStyle.classList.add('btn-success');
   buttonStyle.classList.remove('btn-primary');
+
   let removeStyling = function () {
     buttonStyle.classList.add('btn-primary');
     buttonStyle.classList.remove('btn-success');
   }
+
   eventTimer(removeStyling);
 }
 buttonEvent(stylesToggelBtn);
 
 //add icon + text inside buton on click
 let btnSpinnerIcon = function () {
-  calcButton.innerHTML = '<i class="fas fa-spinner"></i> Calculating... ';
-  let activteText = function () {
+  calcButton.innerHTML = '<i class="fas fa-spinner"></i> Calculating...';
+
+  let endText = function () {
     calcButton.innerHTML = 'Calculate!';
   }
-  eventTimer(activateText);
+
+  eventTimer(endText);
 }
 buttonEvent(btnSpinnerIcon);
 
@@ -48,14 +53,15 @@ buttonEvent(btnSpinnerIcon);
 
 //calculate input
 let calcEvent = function () {
-  let calcInput1 = document.getElementById("formCalculate").elements[0].value;
-  let calcInput2 = document.getElementById("formCalculate").elements[1].value;
+  let calcInput1 = formOutput.elements[0].value;
+  let calcInput2 = formOutput.elements[1].value;
 
   let sum1and2 = parseFloat(calcInput1) + parseFloat(calcInput2);
 
   let printResult = function () {
-    document.getElementById("calcBoard").innerHTML = 'here is your result: <br>   ' + sum1and2;
+    resultInput.innerHTML = '<p class="text-success">Your result =</><br>' + sum1and2 ;
   }
+  
   eventTimer(printResult);
 }
 buttonEvent(calcEvent);
